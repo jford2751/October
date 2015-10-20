ce6e002468af21ec2c4ff366abd7822e0fc9a59f
jford2751 <jford2751@001LAB153010826.neisd.ad>
2015-10-20 10:19:56 -0500

    Added November

---
+++ GitOctober/src/November.java
@@ -0,0 +1,10 @@
+
+public class November {
+	
+public static void main(String[] args) {
+		
+		
+		System.out.println("November is AWEsome");
+		System.out.println("And the weather is awesome!");
+
+}
